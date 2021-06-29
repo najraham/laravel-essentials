626f3fe41a9f98366936f40a3f1f6302f2ec5d27
najraham <anishamaharjan4@gmail.com>
2021-06-29 10:54:45 +0545

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/rooms', 'ShowRoomsController');
+
+//Route::get('/bookings', 'BookingController@index');
+//Route::get('/bookings/create', 'BookingController@create');
+//Route::post('/bookings', 'BookingController@store');
+//Route::get('/bookings/{booking}', 'BookingController@show');
+//Route::get('/bookings/{booking}/edit', 'BookingController@edit');
+//Route::put('/bookings/{booking}', 'BookingController@edit');
+//Route::delete('/bookings/{booking}', 'BookingController@destroy');
+
+Route::resource('bookings', 'BookingController');
